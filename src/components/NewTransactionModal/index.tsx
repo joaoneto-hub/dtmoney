8ee2f59e7408b714f0318d022a4940ed0d087d28
João Rodrigues teles neto <joaonetrs@pop-os.localdomain>
2022-03-02 20:39:36 -0300

    Criação do Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import closeImg from "../../assets/close.svg";
+import icomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import { Container, TransationTypeContainer } from "./styles";
 
 // Modal.setAppElement('root');
 
@@ -18,10 +21,27 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close">
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+
       <Container>
         <h2>Cadastrar Transação</h2>
-        <input placeholder="titulo" />
-        <input type="number" placeholder="valor" />
+        <input placeholder="Titulo" />
+        <input type="number" placeholder="Valor" />
+        <TransationTypeContainer>
+          <button type="button">
+            <img src={icomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saida</span>
+          </button>
+        </TransationTypeContainer>
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
