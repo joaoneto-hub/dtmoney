a7a05043934a64e67e6fb0402dcce49a77e5ecb7
joaoneto-hub <joaordorigues8182@gmail.com>
2022-03-04 15:50:27 -0300

    Criando função de Cadastro de novas transações

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import icomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { Container, TransationTypeContainer } from "./styles";
+import { api } from "../services/api";
+import { Container, TransationTypeContainer, RadioBox } from "./styles";
 
 // Modal.setAppElement('root');
 
@@ -14,6 +16,27 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+
+  const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type
+    }
+
+    api.post('/transactions', data)
+
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -28,21 +51,49 @@ export function NewTransactionModal({
         <img src={closeImg} alt="Fechar Modal" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar Transação</h2>
-        <input placeholder="Titulo" />
-        <input type="number" placeholder="Valor" />
+
+        <input
+          placeholder="Titulo"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
+
         <TransationTypeContainer>
-          <button type="button">
+          <RadioBox
+            type="button"
+            onClick={() => { setType('deposit'); }}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
             <img src={icomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
-          <button type="button">
+          </RadioBox>
+
+
+          <RadioBox
+            type="button"
+            onClick={() => { setType('widthdraw'); }}
+            isActive={type === 'widthdraw'}
+            activeColor="red"
+          >
             <img src={outcomeImg} alt="Saida" />
             <span>Saida</span>
-          </button>
+          </RadioBox>
         </TransationTypeContainer>
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from "polished"
+import { darken, transparentize } from "polished"
 
 export const Container = styled.form`
 h2 {
@@ -60,14 +60,28 @@ export const TransationTypeContainer = styled.div`
   display: grid;
   grid-template-columns:  1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
 
+const colors = {
+
+  green: '#33CC95',
+  red: '#E52e54'
+
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
-    background: transparent;
+    background: ${(props) => props.isActive 
+    ? transparentize(0.9, colors[props.activeColor])
+    : 'transparent' };
 
     display: flex;
     align-items: center;
@@ -76,9 +90,7 @@ export const TransationTypeContainer = styled.div`
     transition: border-color 0.2s;
 
     &:hover {
-
       border-color: ${ darken(0.1, '#d7d7d7')};
-
     }
 
     img {
@@ -94,9 +106,6 @@ export const TransationTypeContainer = styled.div`
       margin-left:  1rem;
       font-size: 1rem;
       color: var(--text-title);
-
-
     }
 
-  }
 `;
